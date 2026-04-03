e9362ca7c52aea5d296bd410724c8379904eba85
junjiejuan <jj.juan@student.han.nl>
2026-04-03 17:05:34 +0200

    unittest van playlistDao aangepast ivm dubbele code

--- src/test/java/nl/han/aim/oose/dea/datasource/dao/PlaylistDaoTest.java
+++ src/test/java/nl/han/aim/oose/dea/datasource/dao/PlaylistDaoTest.java
@@ -27,11 +27,9 @@ class PlaylistDaoTest {
     @InjectMocks
     private PlaylistDao playlistDao;
 
-    @Test
-    void getAllPlaylists_shouldReturnPlaylists_whenDataExists() throws SQLException {
+    // Herbruikbaar tegen dubbele code
+    private void mockPlaylistQueries(Connection connection, PlaylistDTO playlistDTO, int totalLength) throws SQLException {
         // Arrange
-        Connection connection = mock(Connection.class);
-
         PreparedStatement userStmt = mock(PreparedStatement.class);
         PreparedStatement playlistStmt = mock(PreparedStatement.class);
         PreparedStatement lengthStmt = mock(PreparedStatement.class);
@@ -40,32 +38,33 @@ class PlaylistDaoTest {
         ResultSet playlistRs = mock(ResultSet.class);
         ResultSet lengthRs = mock(ResultSet.class);
 
-        PlaylistDTO playlistDTO = new PlaylistDTO(1, "Test", false);
-
-        when(databaseConnector.connect()).thenReturn(connection);
-
-        // user query
-        when(connection.prepareStatement(startsWith("SELECT id FROM users")))
-                .thenReturn(userStmt);
+        // User
+        when(connection.prepareStatement(startsWith("SELECT id FROM users"))).thenReturn(userStmt);
         when(userStmt.executeQuery()).thenReturn(userRs);
         when(userRs.next()).thenReturn(true);
         when(userRs.getInt("id")).thenReturn(1);
 
-        // playlist query
-        when(connection.prepareStatement("SELECT * FROM playlist"))
-                .thenReturn(playlistStmt);
+        // Playlists
+        when(connection.prepareStatement("SELECT * FROM playlist")).thenReturn(playlistStmt);
         when(playlistStmt.executeQuery()).thenReturn(playlistRs);
         when(playlistRs.next()).thenReturn(true, false);
-
         when(playlistMapper.mapToDTO(playlistRs)).thenReturn(playlistDTO);
         when(playlistRs.getInt("user_id")).thenReturn(1);
 
-        // length query
-        when(connection.prepareStatement(startsWith("SELECT COALESCE")))
-                .thenReturn(lengthStmt);
+        // Total length
+        when(connection.prepareStatement(startsWith("SELECT COALESCE"))).thenReturn(lengthStmt);
         when(lengthStmt.executeQuery()).thenReturn(lengthRs);
         when(lengthRs.next()).thenReturn(true);
-        when(lengthRs.getInt("totalLength")).thenReturn(500);
+        when(lengthRs.getInt("totalLength")).thenReturn(totalLength);
+    }
+
+    @Test
+    void getAllPlaylists_shouldReturnPlaylists_whenDataExists() throws SQLException {
+        // Arrange
+        Connection connection = mock(Connection.class);
+        when(databaseConnector.connect()).thenReturn(connection);
+        PlaylistDTO playlistDTO = new PlaylistDTO(1, "Test", false);
+        mockPlaylistQueries(connection, playlistDTO, 500);
 
         // Act
         PlaylistsResponseDTO result = playlistDao.getAllPlaylists("token");
@@ -81,49 +80,24 @@ class PlaylistDaoTest {
     void getAllPlaylists_shouldReturnEmpty_whenNoPlaylistsExist() throws SQLException {
         // Arrange
         Connection connection = mock(Connection.class);
+        when(databaseConnector.connect()).thenReturn(connection);
 
         PreparedStatement userStmt = mock(PreparedStatement.class);
         PreparedStatement playlistStmt = mock(PreparedStatement.class);
         PreparedStatement lengthStmt = mock(PreparedStatement.class);
-
         ResultSet userRs = mock(ResultSet.class);
         ResultSet playlistRs = mock(ResultSet.class);
         ResultSet lengthRs = mock(ResultSet.class);
 
-        when(databaseConnector.connect()).thenReturn(connection);
-
-        // user query
         when(connection.prepareStatement(anyString())).thenReturn(userStmt, playlistStmt, lengthStmt);
-
         when(userStmt.executeQuery()).thenReturn(userRs);
         when(userRs.next()).thenReturn(true);
         when(userRs.getInt("id")).thenReturn(1);
-
-        // geen playlists
         when(playlistStmt.executeQuery()).thenReturn(playlistRs);
         when(playlistRs.next()).thenReturn(false);
-
-        // length
         when(lengthStmt.executeQuery()).thenReturn(lengthRs);
         when(lengthRs.next()).thenReturn(true);
         when(lengthRs.getInt("totalLength")).thenReturn(0);
-
-        // Act
-        PlaylistsResponseDTO result = playlistDao.getAllPlaylists("token");
-
-        // Assert
-        assertNotNull(result);
-        assertEquals(0, result.getPlaylists().size());
-        assertEquals(0, result.getLength());
-    }
-
-    @Test
-    void getAllPlaylists_shouldReturnEmpty_whenSQLExceptionOccurs() throws SQLException {
-        // Arrange
-        Connection connection = mock(Connection.class);
-
-        when(databaseConnector.connect()).thenReturn(connection);
-        when(connection.prepareStatement(anyString())).thenThrow(new SQLException());
 
         // Act
         PlaylistsResponseDTO result = playlistDao.getAllPlaylists("token");
@@ -138,38 +112,15 @@ class PlaylistDaoTest {
     void addPlaylist_shouldReturnUpdatedPlaylists() throws SQLException {
         // Arrange
         Connection connection = mock(Connection.class);
-        PreparedStatement userStatement = mock(PreparedStatement.class);
-        PreparedStatement insertStatement = mock(PreparedStatement.class);
-        ResultSet userResultSet = mock(ResultSet.class);
-
-        PlaylistDTO playlist = new PlaylistDTO(1, "New Playlist", true);
-
         when(databaseConnector.connect()).thenReturn(connection);
 
-        when(connection.prepareStatement(startsWith("SELECT"))).thenReturn(userStatement);
-        when(connection.prepareStatement(startsWith("INSERT"))).thenReturn(insertStatement);
-        when(userStatement.executeQuery()).thenReturn(userResultSet);
-        when(userResultSet.next()).thenReturn(true);
-        when(userResultSet.getInt("id")).thenReturn(1);
-
-        PreparedStatement playlistStmt = mock(PreparedStatement.class);
-        ResultSet playlistRs = mock(ResultSet.class);
-        PreparedStatement lengthStmt = mock(PreparedStatement.class);
-        ResultSet lengthRs = mock(ResultSet.class);
-
         PlaylistDTO playlistDTO = new PlaylistDTO(1, "Test", false);
+        PlaylistDTO playlist = new PlaylistDTO(1, "New Playlist", true);
 
-        when(connection.prepareStatement("SELECT * FROM playlist")).thenReturn(playlistStmt);
-        when(playlistStmt.executeQuery()).thenReturn(playlistRs);
-        when(playlistRs.next()).thenReturn(true, false);
+        PreparedStatement insertStatement = mock(PreparedStatement.class);
+        when(connection.prepareStatement(startsWith("INSERT"))).thenReturn(insertStatement);
 
-        when(playlistMapper.mapToDTO(playlistRs)).thenReturn(playlistDTO);
-        when(playlistRs.getInt("user_id")).thenReturn(1);
-
-        when(connection.prepareStatement(startsWith("SELECT COALESCE"))).thenReturn(lengthStmt);
-        when(lengthStmt.executeQuery()).thenReturn(lengthRs);
-        when(lengthRs.next()).thenReturn(true);
-        when(lengthRs.getInt("totalLength")).thenReturn(0);
+        mockPlaylistQueries(connection, playlistDTO, 0);
 
         // Act
         PlaylistsResponseDTO result = playlistDao.addPlaylist(playlist, "test-token");
@@ -183,40 +134,14 @@ class PlaylistDaoTest {
     void deletePlaylist_shouldReturnUpdatedPlaylists() throws SQLException {
         // Arrange
         Connection connection = mock(Connection.class);
-        PreparedStatement deleteStatement = mock(PreparedStatement.class);
-        PreparedStatement userStatement = mock(PreparedStatement.class);
-        PreparedStatement playlistStmt = mock(PreparedStatement.class);
-        PreparedStatement lengthStmt = mock(PreparedStatement.class);
-
-        ResultSet userRs = mock(ResultSet.class);
-        ResultSet playlistRs = mock(ResultSet.class);
-        ResultSet lengthRs = mock(ResultSet.class);
+        when(databaseConnector.connect()).thenReturn(connection);
 
         PlaylistDTO playlistDTO = new PlaylistDTO(1, "Test Playlist", true);
 
-        when(databaseConnector.connect()).thenReturn(connection);
-
-        // DELETE mock
+        PreparedStatement deleteStatement = mock(PreparedStatement.class);
         when(connection.prepareStatement(startsWith("DELETE"))).thenReturn(deleteStatement);
 
-        // user query mock
-        when(connection.prepareStatement(startsWith("SELECT id FROM users"))).thenReturn(userStatement);
-        when(userStatement.executeQuery()).thenReturn(userRs);
-        when(userRs.next()).thenReturn(true);
-        when(userRs.getInt("id")).thenReturn(1);
-
-        // playlists query mock
-        when(connection.prepareStatement("SELECT * FROM playlist")).thenReturn(playlistStmt);
-        when(playlistStmt.executeQuery()).thenReturn(playlistRs);
-        when(playlistRs.next()).thenReturn(true, false);
-        when(playlistMapper.mapToDTO(playlistRs)).thenReturn(playlistDTO);
-        when(playlistRs.getInt("user_id")).thenReturn(1);
-
-        // total length query mock
-        when(connection.prepareStatement(startsWith("SELECT COALESCE"))).thenReturn(lengthStmt);
-        when(lengthStmt.executeQuery()).thenReturn(lengthRs);
-        when(lengthRs.next()).thenReturn(true);
-        when(lengthRs.getInt("totalLength")).thenReturn(300);
+        mockPlaylistQueries(connection, playlistDTO, 300);
 
         // Act
         PlaylistsResponseDTO result = playlistDao.deletePlaylist(1, "test-token");
@@ -232,42 +157,15 @@ class PlaylistDaoTest {
     void editPlaylist_shouldReturnUpdatedPlaylists() throws SQLException {
         // Arrange
         Connection connection = mock(Connection.class);
-        PreparedStatement updateStatement = mock(PreparedStatement.class);
-        PreparedStatement userStatement = mock(PreparedStatement.class);
-        PreparedStatement playlistStmt = mock(PreparedStatement.class);
-        PreparedStatement lengthStmt = mock(PreparedStatement.class);
-
-        ResultSet userRs = mock(ResultSet.class);
-        ResultSet playlistRs = mock(ResultSet.class);
-        ResultSet lengthRs = mock(ResultSet.class);
-
-        PlaylistDTO playlistDTO = new PlaylistDTO(1, "Updated Playlist", true);
-
-        PlaylistDTO playlist = new PlaylistDTO(1, "Updated Playlist", true);
-
         when(databaseConnector.connect()).thenReturn(connection);
 
-        // UPDATE mock
+        PlaylistDTO playlistDTO = new PlaylistDTO(1, "Updated Playlist", true);
+        PlaylistDTO playlist = new PlaylistDTO(1, "Updated Playlist", true);
+
+        PreparedStatement updateStatement = mock(PreparedStatement.class);
         when(connection.prepareStatement(startsWith("UPDATE"))).thenReturn(updateStatement);
 
-        // user query mock
-        when(connection.prepareStatement(startsWith("SELECT id FROM users"))).thenReturn(userStatement);
-        when(userStatement.executeQuery()).thenReturn(userRs);
-        when(userRs.next()).thenReturn(true);
-        when(userRs.getInt("id")).thenReturn(1);
-
-        // playlists query mock
-        when(connection.prepareStatement("SELECT * FROM playlist")).thenReturn(playlistStmt);
-        when(playlistStmt.executeQuery()).thenReturn(playlistRs);
-        when(playlistRs.next()).thenReturn(true, false);
-        when(playlistMapper.mapToDTO(playlistRs)).thenReturn(playlistDTO);
-        when(playlistRs.getInt("user_id")).thenReturn(1);
-
-        // total length query mock
-        when(connection.prepareStatement(startsWith("SELECT COALESCE"))).thenReturn(lengthStmt);
-        when(lengthStmt.executeQuery()).thenReturn(lengthRs);
-        when(lengthRs.next()).thenReturn(true);
-        when(lengthRs.getInt("totalLength")).thenReturn(300);
+        mockPlaylistQueries(connection, playlistDTO, 300);
 
         // Act
         PlaylistsResponseDTO result = playlistDao.editPlaylist(1, playlist, "test-token");
